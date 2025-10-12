57d5af10da6e11b34917f56b40069fba0806f5c2
RonaldoChambillaRojas <ronaldochambillarojas@gmail.com>
2025-10-12 08:25:27 -0500

    feat (static) Use Static

---
+++ src/10-static.ts
@@ -0,0 +1,19 @@
+console.log(Math.PI);
+console.log(Math.max(1,2,3,4,5,6,2));
+
+class MyMath {
+    static readonly PI = 3.14;
+
+    static max(...numbers: number[]){
+        return numbers.reduce((max, item) => max >= item ? max : item);
+    }
+}
+
+// const newMath = new MyMath();
+// console.log(newMath.PI);
+
+console.log('myMath.PI => ',MyMath.PI);
+// MyMath.PI = 12;
+
+const number = [1,2,34,5,4,34,2,33,45]
+console.log('myMath.max => ', MyMath.max(...number));
