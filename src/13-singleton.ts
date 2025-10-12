722ebafc55f3cddd45e760ef8aa3853b0be35623
RonaldoChambillaRojas <ronaldochambillarojas@gmail.com>
2025-10-12 09:17:02 -0500

    feat (singleton) Use Singleton

---
+++ src/13-singleton.ts
@@ -0,0 +1,29 @@
+class MyService {
+
+    static instance: MyService | null = null;
+
+    private constructor(
+        private name: string,
+    ){}
+
+    getName(){
+        return this.name;
+    }
+
+    static create(name: string) {
+        if(MyService.instance === null){
+            console.log('solo se llama una ves.');
+            MyService.instance = new MyService(name)
+        }
+
+        return MyService.instance;
+    }
+}
+
+const myService1 = MyService.create('service 1');
+console.log(myService1.getName());
+
+const myService2 = MyService.create('service 2');
+console.log(myService2.getName());
+
+console.log(myService1 === myService2);
