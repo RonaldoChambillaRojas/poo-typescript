cbc16475b5ffc445ddbe4fb2266cbec12c9194d7
RonaldoChambillaRojas <ronaldochambillarojas@gmail.com>
2025-10-13 13:10:46 -0500

    feat: fix base http service -- Generics

--- src/app/sevices/base-http.service.ts
+++ src/app/sevices/base-http.service.ts
@@ -3,6 +3,7 @@ import axios from "axios";
 
 import type { Category } from "../models/category.model.js";
 import type { Product } from "../models/product.model.js";
+import type { UpdateProductDto } from "../dtos/product.dto.js";
 
 export class BaseHttpService<TypeClass> {
     // data: TypeClass[] = [];
@@ -12,6 +13,11 @@ export class BaseHttpService<TypeClass> {
     ){}
     async getAll(): Promise<TypeClass[]>{
         const {data} = await axios.get<TypeClass[]>(this.url);
+        return data;
+    }
+
+    async update<ID, DTO>(id: ID, changes: DTO){
+        const { data } = await axios.put(`${this.url}/${id}`, changes);
         return data;
     }
 }
@@ -27,9 +33,15 @@ export class BaseHttpService<TypeClass> {
     const products = await productService.getAll()
     console.log('products:::::::::::::::::', products.length);
 
+    productService.update<Product['id'], UpdateProductDto>(1,{
+        title: 'hola nuevo title',
+    });
+
 
     const url2 = 'https://api.escuelajs.co/api/v1/categories'
     const productService2 = new BaseHttpService<Product>(url2);
     const categorys = await productService2.getAll()
     console.log('Categories:::::::::::::::::::::::::', categorys);
+
+    // productService2.update<Category['id',]>();
 })();
