1108e02ed9312007b896bb1cc3bbe8ce08038ad5
RonaldoChambillaRojas <ronaldochambillarojas@gmail.com>
2025-10-12 23:23:34 -0500

    feat (generics) Use Generics

---
+++ src/15-generics.ts
@@ -0,0 +1,19 @@
+// function getValue(value: unknown){
+//     return value;
+// }
+
+// function getValue(value: string | number){
+//     return value;
+// }
+
+// getValue(12).toFixed();
+// getValue('12').toFixed();
+
+function getValue<myType>(value: myType){
+    return value;
+}
+
+getValue<number>(12).toFixed();
+getValue<string>('12').concat();
+getValue<number[]>([]).forEach;
+
